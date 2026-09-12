0cd855c38ea087e449288ed90c47ba544bd5b2aa
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-12 02:04:20 +0200

    Create coreAnimations.jsx

---
+++ src/vgpu/swiftui-bridge/coreAnimations.jsx
@@ -0,0 +1,4 @@
+swift.addAnimation(node, CoreAnimation.fadeIn(0.4));
+swift.addAnimation(node, CoreAnimation.moveTo(100, 200, 0.6));
+swift.addAnimation(node, CoreAnimation.scaleTo(1.5, 0.3));
+swift.addAnimation(node, CoreAnimation.spring({ duration: 0.8 }));
