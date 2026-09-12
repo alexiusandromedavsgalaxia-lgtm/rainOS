cc04ad6c15d19d6a83f93c766f3c34e12fb6ce94
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-12 14:41:28 +0200

    Create index.js

---
+++ src/system/index.js
@@ -0,0 +1,6 @@
+// src/system/index.js
+export * from "./syscalls.jsx";
+export * from "./syslogs.jsx";
+export * from "./sysdiagnose.jsx";
+export * from "./sysfiltered.jsx";
+export * from "./sysbatdiagnostic.jsx";
